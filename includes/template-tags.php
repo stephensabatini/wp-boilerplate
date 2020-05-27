c31630d4c2cb11ca2623a24e0eae529927a195d3
Stephen Sabatini <info@stephensabatini.com>
2020-05-26 21:16:06 -0400

    Add template-tags.php.

---
+++ includes/template-tags.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template Tags
+ *
+ * The template tags for this theme.
+ *
+ * @author Stephen Sabatini <info@stephensabatini.com>
+ * @package Boilerplate\Template_Tags
+ * @version 1.0.0
+ * @license GPL, or GNU General Public License, version 2
+ */
+
+namespace Boilerplate\Template_Tags;
