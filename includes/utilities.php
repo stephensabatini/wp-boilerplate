72198e9249b1467ff38da9e4c4fc212465fb5896
Stephen Sabatini <info@stephensabatini.com>
2020-05-26 21:16:16 -0400

    Add utilities.php.

---
+++ includes/utilities.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Utilities
+ *
+ * The utility functions for this theme.
+ *
+ * @author Stephen Sabatini <info@stephensabatini.com>
+ * @package Boilerplate\Utilities
+ * @version 1.0.0
+ * @license GPL, or GNU General Public License, version 2
+ */
+
+namespace Boilerplate\Utilities;
