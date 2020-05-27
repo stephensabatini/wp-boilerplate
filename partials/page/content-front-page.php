ddb29b65e42cf4a80fb0c9ab09236d9acce6b6f8
Stephen Sabatini <info@stephensabatini.com>
2020-05-26 21:18:42 -0400

    Add content-front-page.php

---
+++ partials/page/content-front-page.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Displays content for front page
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ * @author Stephen Sabatini <info@stephensabatini.com>
+ * @package WP-Boilerplate
+ * @version 1.0.0
+ * @license GPL, or GNU General Public License, version 2
+ */
+?>
+<main id="main-wrapper" class="site-main-wrapper" role="main" itemprop="mainContentOfPage" itemscope itemtype="http://schema.org/WebPageElement">
+    <article class="site-main">
+        <?php
+        /* translators: %s: Name of current post */
+        the_content( sprintf(
+            __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'stephensabatini' ),
+            get_the_title()
+        ) );
+        ?>
+    </article>
+</main>
