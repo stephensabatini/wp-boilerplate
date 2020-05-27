1b03f6aa0fb17d24d8033cce9ff101bcb8c618d6
Stephen Sabatini <info@stephensabatini.com>
2020-05-26 21:36:44 -0400

    Add sticky class to header on scroll. Up to you to use it or not.

---
+++ src/js/sticky-content.js
@@ -0,0 +1,14 @@
+var stickyClass = "sticky";
+var scrollpos = window.scrollY;
+var nav = document.getElementById("main-navigation-wrapper");
+var header = document.getElementById("site-header");
+
+window.addEventListener("scroll", function() {
+    scrollpos = window.scrollY;
+
+    if (scrollpos > (header.offsetHeight - nav.offsetHeight)) {
+        nav.classList.add(stickyClass);
+    } else {
+        nav.classList.remove(stickyClass);
+    }
+});
