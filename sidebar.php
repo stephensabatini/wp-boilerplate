053a54f7c36f1a4c1e635e5029f27c9e92451300
Stephen Sabatini <info@stephensabatini.com>
2020-05-26 21:24:27 -0400

    Add sidebar.php.

---
+++ sidebar.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The sidebar containing the main widget area
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ * @author Stephen Sabatini <info@stephensabatini.com>
+ * @package WP-Boilerplate
+ * @version 1.0.0
+ * @license GPL, or GNU General Public License, version 2
+ */
+
+if ( ! is_active_sidebar( 'default' ) ) {
+	return;
+}
+?>
+<aside id="sidebar" class="sidebar-content widget-area" role="complementary" aria-label="<?php esc_attr_e( 'Sidebar', BOILERPLATE_TEXT_DOMAIN ); ?>">
+	<?php dynamic_sidebar( 'default' ); ?>
+</aside><!-- #sidebar -->
