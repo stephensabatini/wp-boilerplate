56224323d4b4963215f924d9af6a9eca547e0310
Stephen Sabatini <info@stephensabatini.com>
2020-05-26 21:17:49 -0400

    Add page.php.

---
+++ page.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Page Template
+ *
+ * This is the default template for all pages.
+ *
+ * @link https://developer.wordpress.org/themes/template-files-section/page-template-files/
+ * @author Stephen Sabatini <info@stephensabatini.com>
+ * @package WP-Boilerplate
+ * @version 1.0.0
+ * @license GPL, or GNU General Public License, version 2
+ */
+
+get_header();
+
+if ( have_posts() ) {
+    while ( have_posts() ) {
+        the_post();
+        get_template_part( 'partials/page/content', 'page' );
+
+        // If comments are open or we have at least one comment, load up the comment template.
+        if ( comments_open() || get_comments_number() )  {
+            comments_template();
+        }
+    }
+} else {
+    get_template_part( 'partials/post/content', 'none' );
+}
+
+get_sidebar();
+get_footer();
