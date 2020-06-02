48e8c9662e32571972091ed50662bab0edc16a35
Stephen Sabatini <info@stephensabatini.com>
2020-06-02 13:54:51 -0400

    Syntax improvements. Remove manifest.json. Move JS detection to overrides.php.

--- includes/core.php
+++ includes/core.php
@@ -19,17 +19,15 @@ namespace Boilerplate\Core;
  * @return void
  */
 function setup() {
-    $n = function( $function ) {
-        return __NAMESPACE__ . "\\$function";
-    };
+	$n = function( $function ) {
+		return __NAMESPACE__ . "\\$function";
+	};
 
-    add_action( 'after_setup_theme', $n( 'i18n' ) );
-    add_action( 'after_setup_theme', $n( 'theme_setup' ) );
-    add_action( 'widgets_init', $n( 'widgets' ) );
-    add_action( 'wp_head', $n( 'js_detection' ), 0 );
-    add_action( 'wp_head', $n( 'add_manifest' ), 10 );
-    add_action( 'wp_enqueue_scripts', $n( 'scripts' ) );
-    add_action( 'wp_enqueue_scripts', $n( 'styles' ) );
+	add_action( 'after_setup_theme', $n( 'i18n' ) );
+	add_action( 'after_setup_theme', $n( 'theme_setup' ) );
+	add_action( 'widgets_init', $n( 'widgets' ) );
+	add_action( 'wp_enqueue_scripts', $n( 'scripts' ) );
+	add_action( 'wp_enqueue_scripts', $n( 'styles' ) );
 }
 
 /**
@@ -40,77 +38,74 @@ function setup() {
  * @return void
  */
 function i18n() {
-    load_theme_textdomain( BOILERPLATE_TEXT_DOMAIN, BOILERPLATE_PATH . '/languages' );
+	load_theme_textdomain( BOILERPLATE_TEXT_DOMAIN, BOILERPLATE_PATH . '/languages' );
 }
 
 /**
  * Sets up theme defaults and registers support for various WordPress features.
  */
 function theme_setup() {
+	add_theme_support( 'automatic-feed-links' );
+	add_theme_support( 'title-tag' );
 
-    add_theme_support( 'automatic-feed-links' );
-    add_theme_support( 'title-tag' );
+	// Enable featured images.
+	add_theme_support( 'post-thumbnails' );
 
-    // Enable featured images.
-    add_theme_support( 'post-thumbnails' );
+	// Take advantage of HTML5.
+	add_theme_support(
+		'html5', [
+			'caption',
+			'comment-list',
+			'comment-form',
+			'gallery',
+			'search-form'
+		]
+	);
 
-    // Take advantage of HTML5.
-    add_theme_support(
-        'html5', [
-            'caption',
-            'comment-list',
-            'comment-form',
-            'gallery',
-            'search-form'
-        ]
-    );
+	add_theme_support( 'post-formats', array(
+		#'aside',
+		#'gallery',
+		#'link',
+		#'image',
+		#'quote',
+		#'status',
+		#'video'
+	) );
 
-    add_theme_support( 'post-formats', array(
-        #'aside',
-        #'gallery',
-        #'link',
-        #'image',
-        #'quote',
-        #'status',
-        #'video'
-    ) );
+	// Define our branding colors.
+	add_theme_support(
+		'editor-color-palette', [
+			[
+				'name' => __( 'Primary Color', BOILERPLATE_TEXT_DOMAIN ),
+				'slug' => 'primary',
+				'color' => '#f68a1f',
+			],
+			[
+				'name' => __( 'Secondary Color', BOILERPLATE_TEXT_DOMAIN ),
+				'slug' => 'secondary',
+				'color' => '#116581',
+			],
+			[
+				'name' => __( 'Tertiary Color', BOILERPLATE_TEXT_DOMAIN ),
+				'slug' => 'tertiary',
+				'color' => '#222',
+			],
+			[
+				'name' => __( 'Quaternary Color', BOILERPLATE_TEXT_DOMAIN ),
+				'slug' => 'quaternary',
+				'color' => '#fff',
+			]
+		]
+	);
 
-    // Define our branding colors for the Gutenberg editor in WP 5+.
-    add_theme_support(
-        'editor-color-palette', [
-            [
-                'name'  => __( 'Primary Color', BOILERPLATE_TEXT_DOMAIN ),
-                'slug' => 'primary',
-                'color' => '#f68a1f',
-            ],
-            [
-                'name'  => __( 'Secondary Color', BOILERPLATE_TEXT_DOMAIN ),
-                'slug' => 'secondary',
-                'color' => '#116581',
-            ],
-            [
-                'name'  => __( 'Tertiary Color', BOILERPLATE_TEXT_DOMAIN ),
-                'slug' => 'tertiary',
-                'color' => '#222',
-            ],
-            [
-                'name'  => __( 'Quaternary Color', BOILERPLATE_TEXT_DOMAIN ),
-                'slug' => 'quaternary',
-                'color' => '#fff',
-            ]
-        ]
-    );
+	add_theme_support( 'editor-styles' );
+	add_editor_style( BOILERPLATE_TEMPLATE_URL . '/dist/css/index.min.css' );
 
-    add_theme_support( 'editor-styles' );
-    add_editor_style( BOILERPLATE_TEMPLATE_URL . '/dist/css/index.min.css' );
-
-    // Register navigation menus for wp_nav_menu().
-    register_nav_menus( [
-        'main-navigation'   => __( 'Main Navigation', BOILERPLATE_TEXT_DOMAIN ),
-        'social-navigation' => __( 'Social Navigation', BOILERPLATE_TEXT_DOMAIN )
-    ] );
-
-    add_image_size( 'large', 1046 );
+	// Register navigation menus for wp_nav_menu().
+	register_nav_menus( [
+		'main-navigation'   => __( 'Main Navigation', BOILERPLATE_TEXT_DOMAIN ),
+		'social-navigation' => __( 'Social Navigation', BOILERPLATE_TEXT_DOMAIN )
+	] );
 }
 
 /**
@@ -119,10 +114,10 @@ function theme_setup() {
  * @return void
  */
 function scripts() {
-    wp_enqueue_script( 'site', BOILERPLATE_TEMPLATE_URL . '/dist/js/index.min.js', [], BOILERPLATE_VERSION, true );
-    if ( is_singular() && comments_open() && '1' === get_option('thread_comments') ) {
-        wp_enqueue_script('comment-reply');
-    }
+	wp_enqueue_script( 'site', BOILERPLATE_TEMPLATE_URL . '/dist/js/index.min.js', [], BOILERPLATE_VERSION, true );
+	if ( is_singular() && comments_open() && '1' === get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
 }
 
 /**
@@ -131,7 +126,7 @@ function scripts() {
  * @return void
  */
 function styles() {
-    wp_enqueue_style( 'site', BOILERPLATE_TEMPLATE_URL . '/dist/css/index.min.css', [], BOILERPLATE_VERSION );
+	wp_enqueue_style( 'site', BOILERPLATE_TEMPLATE_URL . '/dist/css/index.min.css', [], BOILERPLATE_VERSION );
 }
 
 /**
@@ -140,32 +135,12 @@ function styles() {
  * @return void
  */
 function widgets() {
-    register_sidebar( [
-        'id'            => 'default',
-        'class'         => 'default',
-        'name'          => __( 'Sidebar: Default Template', BOILERPLATE_TEXT_DOMAIN ),
-        'description'   => __( 'The area for the right column of the default, full-width, and narrow-width template pages.', BOILERPLATE_TEXT_DOMAIN ),
-        'before_widget' => '<div id="%1$s" class="widget %2$s">',
-        'after_widget'  => '</div>'
-    ] );
-}
-
-/**
- * Handles JavaScript detection.
- *
- * Adds a `js` class to the root `<html>` element when JavaScript is detected.
- *
- * @return void
- */
-function js_detection() {
-    echo "<script>(function(html){html.className = html.className.replace(/\bno-js\b/,'js')})(document.documentElement);</script>\n";
-}
-
-/**
- * Appends a link tag used to add a manifest.json to the head
- *
- * @return void
- */
-function add_manifest() {
-    echo "<link rel='manifest' href='" . esc_url( BOILERPLATE_TEMPLATE_URL . '/manifest.json' ) . "' />";
+	register_sidebar( [
+		'id'            => 'default',
+		'class'         => 'default',
+		'name'          => __( 'Sidebar: Default Template', BOILERPLATE_TEXT_DOMAIN ),
+		'description'   => __( 'The area for the right column of the default, full-width, and narrow-width template pages.', BOILERPLATE_TEXT_DOMAIN ),
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</div>'
+	] );
 }
